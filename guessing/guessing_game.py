a03c49a5702d71b5897e1eaf6d541c5291c79d0a
Laurence May <laurence.e.may@gmail.com>
2014-09-22 08:20:07 +0100

    working game

--- guessing/guessing_game.py
+++ guessing/guessing_game.py
@@ -7,9 +7,9 @@ guess = 0
 
 print('Access DENIED: this system is locked with a code between 1-99')
 
-while guess != secret and attemps < tries:
+while guess != secret and attempts < tries:
     remaining = tries - attempts
-    print('you have' + str(remaining) + 'remaining attempts')
+    print('you have ' + str(remaining) + ' remaining attempts')
     guess = int(input("enter code now..."))
     if guess < secret:
         print("code: FAIL:  Too low. don't you know any smaller numbers")
@@ -17,6 +17,16 @@ while guess != secret and attemps < tries:
         print("code: FAIL:  Too high. Cool your boots and try something smaller")
     attempts = attempts + 1
 
-if guess==secret:
+if guess == secret:
     print("Confirmed")
+    print("Access: ENABLED. Please proceed to the Nuclear Launch Stations. Have a nice day")
+else:
+    print("Access: LOCKED: Black helicopters have been sent to your location")
+    print("Before you are terminated we would like you to know that the number was " + str(secret))
+
+
+
+
+
+
 
