473d4c39b812c22177a6375c1fc2927b7d47b188
Laurence May <laurence.e.may@gmail.com>
2014-10-01 20:39:09 +0100

    working version with validation

--- guessing/NumberGuessingEdit.py
+++ guessing/NumberGuessingEdit.py
@@ -1,26 +1,23 @@
+
 import random,time
 tries = 6
 attempts = 0
 secret = random.randint(1, 99)
 guess = 0
 
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
 
 def inputNumber():
     try:
-        number =  int(raw_input("enter code now: "))
-        if number>99 or number<1:
-            print("pick a number between 1 and 99 you IDIOT")
+        user_input = raw_input("enter a number between 1 and 99: ")
+        n = int(user_input)
+        if n>99 or n<1:
+            print("You are an IDIOT")
             time.sleep(0.25)
             return inputNumber()
-        return number
+        else:
+            return n
     except ValueError:
-        print("You are an IDIOT, type in a number between 1 and 99")
+        print("You are an IDIOT")
         time.sleep(0.25)
         return inputNumber()
 
@@ -31,14 +28,6 @@ while guess != secret and attempts < tries:
     remaining = tries - attempts
 
     print('you have ' + str(remaining) + ' remaining attempts')
-
-#    guess_string = raw_input("enter code now...")
-#
-#    if (is_number(guess_string)):
-#        guess = int(guess_string)
-#    else:
-#        print("You are an IDIOT, type in a number between 1 and 99")
-#        attempts = attempts - 1
 
     guess = inputNumber()
     if guess < secret:
